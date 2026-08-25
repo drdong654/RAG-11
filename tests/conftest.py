e2da015f100a08f861422675c2012a9068c352e4
drdong654 <vanjdamanj@gmail.com>
2026-08-26 01:03:12 +0300

    fix ci/cd #2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,58 @@
-import pytest
-from pathlib import Path
+import os
 
-from services import RegistrationService, UserStorage
+import pytest_asyncio
+
+from bot.db.repositories.users import UserRepository
+from bot.db.engine import make_sessionmaker
+from bot.db.models import Base
+from dotenv import load_dotenv
+from services import UserStorage
+
+load_dotenv()
+
+TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
+
+if not TEST_DATABASE_URL: 
+    raise RuntimeError("TEST_DATABASE_URL is not set")
+
+#Создаём UserRepository с тестовой сессией БД
+@pytest_asyncio.fixture
+async def user_repository(db_session):
+    repository = UserRepository(db_session)
+    yield repository
 
 
+@pytest_asyncio.fixture
+async def user_storage():
+    engine, Session = make_sessionmaker(TEST_DATABASE_URL)
 
-@pytest.fixture
-def user_storage(tmp_path):
-    db = tmp_path / "users.db"
-    return UserStorage(db)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
-@pytest.fixture
-def registration_service(user_storage):
-    return RegistrationService(user_storage)
+    try:
+        yield UserStorage(Session)
+
+    finally:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+
+        await engine.dispose()
+
+#Создаем таблицу и открываем сессию БД
+@pytest_asyncio.fixture
+async def db_session():
+    engine, Session = make_sessionmaker(TEST_DATABASE_URL)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    try:
+        async with Session() as session:
+            yield session
+
+    finally:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+
+        await engine.dispose()
+    
